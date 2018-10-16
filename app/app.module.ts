f2046c2f413c68f14e350dfab3dfbb28d6242190
krobpfg <44204524+krobpfg@users.noreply.github.com>
2018-10-16 12:29:49 -0500

    Add files via upload
    New components

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
