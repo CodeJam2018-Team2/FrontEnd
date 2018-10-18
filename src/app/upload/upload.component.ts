e51afdbacd9407c1e6966488527030c83aa164d1
krobpfg <44204524+krobpfg@users.noreply.github.com>
2018-10-18 10:43:23 -0500

    Update upload.component.ts

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload',
-  templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  templateUrl: './upload.component.html'
 })
 export class UploadComponent implements OnInit {
 
